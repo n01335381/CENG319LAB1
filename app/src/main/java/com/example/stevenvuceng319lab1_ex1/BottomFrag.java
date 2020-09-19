03de8ba1d34f869d1bebc10da1fd5daab6fd3069
n01335381 <71400096+n01335381@users.noreply.github.com>
2020-09-19 19:46:25 -0400

    initial commit of lab 01

---
+++ app/src/main/java/com/example/stevenvuceng319lab1_ex1/BottomFrag.java
@@ -0,0 +1,4 @@
+package com.example.stevenvuceng319lab1_ex1;
+
+public class BottomFrag {
+}
---
+++ app/src/main/java/com/example/stevenvuceng319lab1_ex1/TopFrag.java
@@ -0,0 +1,32 @@
+package com.example.stevenvuceng319lab1_ex1;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.fragment.app.ListFragment;
+
+public class TopFrag extends ListFragment {
+
+    String[] activities = new String[]
+        {
+                "AIActivity",
+                "VRActivity"
+        };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view =inflater.inflate(R.layout.top_frag, container, false);
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+            android.R.layout.simple_list_item_1,activities);
+        //ArrayAdapter calls toString() on each array item and places in a TextView
+        //this creates a view for each array item
+    setListAdapter(adapter); //binding list view to the array adapter
+
+    return view;
+    }
+
+}
